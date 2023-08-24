013d03dcd9c10b3bb733f6b46de3676661e34be5
Mahadev Godbole <godbolemahadevkanka@gmail.com>
2023-08-25 00:12:08 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@
 // index.js
 const {BadRequestError} = require("./errors/bad-request-error");
 const {CustomError} = require("./errors/custom-error");
-const {DatabaseConnectionError} = require("./errors/database-connection-error");
+const {DatabaseConnectionError} = require("./errors/database-connection-errors");
 const {NotAuthorizedError} = require("./errors/not-authorized-error");
 const {NotFoundError} = require("./errors/not-found-error");
 const {RequestValidationError} = require("./errors/request-validation-errors");
@@ -23,7 +23,7 @@ const {RequestValidationError} = require("./errors/request-validation-errors");
 const {currentUser} = require("./middleware/currentuser");
 const {errorHandler} = require("./middleware/error-handler");
 const {requireAuth} = require("./middleware/require-auth");
-const {validationRequest} = require("./middleware/validation-request");
+const {validateRequest} = require("./middleware/validation-request");
 
 module.exports = {
   BadRequestError,
@@ -35,6 +35,6 @@ module.exports = {
   currentUser,
   errorHandler,
   requireAuth,
-  validationRequest,
+  validateRequest
 };
 
